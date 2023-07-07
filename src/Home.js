0672efd23705442a378eb17c744aff12cf7b23fc
risingone <yashprakashsharma2000@gmail.com>
2023-07-07 22:49:32 +0530

    Listing the Blogs at HomePage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'yash', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'prakash', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'yash', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>Home Page!</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written be { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
